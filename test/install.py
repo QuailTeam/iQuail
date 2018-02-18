1a86af7a6bcf98aa157e06d7d8ceca42a0c4b43a
Arnaud Aliès <arnaud.alies@epitech.eu>
2018-02-18 18:22:09 +0100

    wip

--- test/install.py
+++ test/install.py
@@ -3,25 +3,60 @@
 import configparser
 import pathlib
 import os.path
-
+import shutil
 # poc install linux
 
-def install(binary):
-    config = configparser.ConfigParser()
-    config.optionxform=str
-    config['Desktop Entry'] = {
-        'Name': 'testapp',
-        'Exec': '/home/mou/perso/Quail/test/install.py',
-        'Icon': '/home/mou/perso/Quail/test/icon.jpeg',
-        'Terminal': 'true',
-        'Type': 'Application'
+'''
+Notes for all user install:
+
+path = os.path.join(os.sep, "usr", "share", "applications",
+                            "%s.desktop" % (binary))
+
+Files /opt
+
+'''
+
+class LinuxInstaller:
+    def __init__(self, app_name, solution_path, binary, icon, console=False):
+        self.solution_path = solution_path
+        self.name = app_name
+        self.icon = icon
+        self.binary = binary
+        self.console = console
+
+    def _copy_files(self):
+        dst = os.path.join(pathlib.Path.home(), '.quail', self.name)
+        if os.path.exists(dst):
+            shutil.rmtree(dst)
+        shutil.copytree(self.solution_path, dst)
+        return dst
+
+    def _register_app(self, path):
+        config = configparser.ConfigParser()
+        config.optionxform=str
+        config['Desktop Entry'] = {
+            'Name': self.name,
+            'Path': path,
+            'Exec': os.path.join(path, self.binary),
+            'Icon': os.path.join(path, self.icon),
+            'Terminal': 'true' if self.console else 'false',
+            'Type': 'Application'
         }
-    
-    path = os.path.join(pathlib.Path.home(),
+        path = os.path.join(pathlib.Path.home(),
                         ".local", "share", "applications",
-                        "%s.desktop" % (binary))
-    with open(path, "w") as f:
-        config.write(f)
+                        "%s.desktop" % (name))
+
+        with open(path, "w") as f:
+            config.write(f)
+
+    def user_install(self):
+        dst = self._copy_files()
+        self._register_app(dst)
 
 if (__name__ == '__main__'):
-    install("testapp")
+    solution_path = '/home/mou/perso/quail/test/test_solution/'
+    name = 'Lolallum1'
+    icon = 'icon.jpeg'
+    binary = 'allum1'
+    installer = LinuxInstaller(name, solution_path, binary, icon, console=True)
+    installer.user_install()
