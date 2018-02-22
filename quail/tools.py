6c305b9b6067e3ee8a3798e4208a54a4ab99bf51
Arnaud Aliès <arnaud.alies@epitech.eu>
2018-02-22 23:09:43 +0100

    ok

--- quail/tools.py
+++ quail/tools.py
@@ -3,15 +3,6 @@ import sys
 import ctypes
 
 
-def rerun_as_admin():
-    if not ctypes.windll.shell32.IsUserAnAdmin():
-        ctypes.windll.shell32.ShellExecuteW(None,
-                                            'runas',
-                                            sys.executable,
-                                            ' '.join(sys.argv),
-                                            None, 1)
-        exit(0)
-
 def get_module_path():
     return os.path.dirname(os.path.realpath(__file__))
 
@@ -31,3 +22,17 @@ def get_script_path():
 def run_from_script():
     '''check if being run from script and not builded in standalone binary'''
     return get_script().endswith(".py")
+
+
+def rerun_as_admin():
+    if (platform.system() == 'Linux'):
+        # os.system('pkexec %s %s' % (get_script_path(), ' '.join(sys.argv[1:])))
+        raise NotImplementedError
+    elif (platform.system() == 'Windows'):
+        if not ctypes.windll.shell32.IsUserAnAdmin():
+            ctypes.windll.shell32.ShellExecuteW(None,
+                                                'runas',
+                                                sys.executable,
+                                                ' '.join(sys.argv),
+                                                None, 1)
+    exit(0)
