dd63ae1c9da153cca5dc8e5ec08e6dc94088adda
Charles-Edouard Lecat <charles-edouard.lecat@epitech.eu>
2018-03-07 16:38:11 +0000

    setup corrected norm

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
+"""Setup generator file."""
 from distutils.core import setup
 
 setup(
-    name = 'quail',
-    packages = ['quail'],
-    version = '1.0',
-    description = 'Quail cross-platform install builder',
-    author = 'Quail team',
-    author_email = 'arnaudalies.py@gmail.com',
-    url = 'https://github.com/mouuff/Quail',
-    download_url = 'https://github.com/mouuff/Quail/tarball/master',
-    keywords = ['tool', 'deploy', 'installer', 'wizard', 'install'],
-    classifiers = [],
+    name='quail',
+    packages=['quail'],
+    version='1.0',
+    description='Quail cross-platform install builder',
+    author='Quail team',
+    author_email='arnaudalies.py@gmail.com',
+    url='https://github.com/mouuff/Quail',
+    download_url='https://github.com/mouuff/Quail/tarball/master',
+    keywords=['tool', 'deploy', 'installer', 'wizard', 'install'],
+    classifiers=[],
     install_requires=[
         'pyinstaller'
     ],
