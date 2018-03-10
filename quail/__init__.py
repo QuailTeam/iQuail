c47b6994e527355d6ba20045574328c52d2f3d2a
Arnaud Aliès <arnaud.alies@epitech.eu>
2018-03-10 19:19:19 +0100

    wip

--- quail/__init__.py
+++ quail/__init__.py
@@ -4,15 +4,6 @@ from .run import run
 from .solution_local import SolutionLocal
 from .solution_ftp import SolutionFtp
 from .solution_zip import SolutionZip
+from .installer import Installer
 from . import helper
 from . import builder
-
-
-if helper.OS_LINUX:
-    from .installer_linux import InstallerLinux
-    Installer = InstallerLinux
-elif helper.OS_WINDOWS:
-    from .installer_windows import InstallerWindows
-    Installer = InstallerWindows
-else:
-    raise NotImplementedError
