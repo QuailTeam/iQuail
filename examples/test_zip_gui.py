64e3c12a062574ee2220935432b13da6eb40116a
Arthur Josso <arthur.josso@epitech.eu>
2018-03-15 15:06:08 +0100

    zip gui test

---
+++ examples/test_zip_gui.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import os
+import os.path
+import quail
+import platform
+
+if quail.helper.OS_LINUX:
+    solution_path = ['Allum1']
+    quail.run(
+        quail.InstallerGui(
+            name='LolAllum1',
+            icon='icon.jpeg',
+            binary='allum1',
+            solution=quail.SolutionZip('solution.zip'),
+            console=True
+        ),
+        quail.builder.Builder(
+            quail.builder.CmdZip(solution_path, 'solution.zip')
+        )
+    )
+
+if quail.helper.OS_WINDOWS:
+    solution_path = ['OpenHardwareMonitor']
+    quail.run(
+        quail.InstallerGui(
+            name='OpenHardwareMonitor',
+            icon='OpenHardwareMonitor.exe',
+            binary='OpenHardwareMonitor.exe',
+            solution=quail.SolutionZip('solution.zip'),
+            console=True
+        ),
+        quail.builder.Builder(
+            quail.builder.CmdIcon('icon.ico'),
+            quail.builder.CmdZip(solution_path, 'solution.zip')
+        )
+    )
