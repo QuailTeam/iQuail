f4ac57fcbe3705f802aaf4acaaf4f172923d5143
Arthur Josso <arthur.josso@epitech.eu>
2018-03-19 13:41:16 +0100

    GUI basics

--- quail/installer_gui.py
+++ quail/installer_gui.py
@@ -1,40 +1,86 @@
 #!/usr/bin/python3
 
 import sys
+from PyQt5.QtCore import (QObject, QThread, pyqtSlot, pyqtSignal)
 from PyQt5.QtWidgets import (QWidget, QToolTip, 
-    QPushButton, QApplication)
+    QPushButton, QApplication, QLabel, QVBoxLayout, QHBoxLayout)
 from PyQt5.QtGui import QFont    
 from .installer import Installer
 
 class Gui(QWidget):
-    def __init__(self, installer):
+    def __init__(self, installer, app):
         super().__init__()
         self.installer = installer
-        self.initUI()
+        self.app = app
+        self._initUI()
+        self.thread = QThread()
         
-    def initUI(self):
+    def _initUI(self):
         
-        QToolTip.setFont(QFont('SansSerif', 10))
-        
-        btn = QPushButton('Install', self)
-        btn.clicked.connect(self.installer._installButton)
-        btn.setToolTip('install the application')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
-        
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Quail')    
-        self.show()
+        self.state = QLabel('Not installed', self)
 
-class InstallerGui(Installer):
+        self.btn = QPushButton('Install', self)
+        self.btn.clicked.connect(self._startInstall)
+        
+        self.btn_exit = QPushButton('Exit', self)
+        self.btn_exit.clicked.connect(self._exit)
+        
+        hbox_bottom = QHBoxLayout()
+        hbox_bottom.addWidget(self.btn)
+        hbox_bottom.addWidget(self.btn_exit)
+        
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(self.state)
+        vbox.addLayout(hbox_bottom)
+        self.setLayout(vbox)
+        
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Quail')
+        
+        self.installer.sig_done.connect(self._on_install_done)
+        self.app.aboutToQuit.connect(self._atexit)
+        self.show()
+        
+    def _startInstall(self):
+        self.btn.setDisabled(True)
+        self.btn_exit.setDisabled(True)
+        self.state.setText('Installing...')
+        self.installer.moveToThread(self.thread)
+        self.thread.started.connect(self.installer._runInstall)
+        self.thread.start()
     
+    def _exit(self):
+        self._on_exit()
+        self.app.quit()
+    
+    def _on_install_done(self):
+        self.state.setText('Installed')
+        self.btn_exit.setEnabled(True)
+        self._on_exit()
+    
+    def _atexit(self):
+        self._on_exit()
+    
+    def _on_exit(self):
+        if self.thread.isRunning():
+            self.thread.quit()
+            self.thread.wait()
+
+
+class InstallerGui(Installer, QObject):
+    
+    sig_done = pyqtSignal()
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        Installer.__init__(self, *args, **kwargs)
+        QObject.__init__(self)
         
     def install(self):
         app = QApplication([])
-        gui = Gui(self)
-        sys.exit(app.exec_())
+        gui = Gui(self, app)
+        app.exec_()
     
-    def _installButton(self):
-        super().install()
+    def _runInstall(self):
+        Installer.install(self)
+        self.sig_done.emit()
+
