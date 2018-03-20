93cdbff1447f5b9d2ec001f78dfc26af7eac4fe7
Arthur Josso <arthur.josso@epitech.eu>
2018-03-20 15:41:38 +0100

    Qt to tkinter transition

--- quail/installer_gui.py
+++ quail/installer_gui.py
@@ -1,86 +1,36 @@
 #!/usr/bin/python3
 
 import sys
-from PyQt5.QtCore import (QObject, QThread, pyqtSlot, pyqtSignal)
-from PyQt5.QtWidgets import (QWidget, QToolTip, 
-    QPushButton, QApplication, QLabel, QVBoxLayout, QHBoxLayout)
-from PyQt5.QtGui import QFont    
+from threading import Thread
+from tkinter import Button, Label, Tk, messagebox
 from .installer import Installer
 
-class Gui(QWidget):
-    def __init__(self, installer, app):
-        super().__init__()
-        self.installer = installer
-        self.app = app
-        self._initUI()
-        self.thread = QThread()
-        
-    def _initUI(self):
-        
-        self.state = QLabel('Not installed', self)
-
-        self.btn = QPushButton('Install', self)
-        self.btn.clicked.connect(self._startInstall)
-        
-        self.btn_exit = QPushButton('Exit', self)
-        self.btn_exit.clicked.connect(self._exit)
-        
-        hbox_bottom = QHBoxLayout()
-        hbox_bottom.addWidget(self.btn)
-        hbox_bottom.addWidget(self.btn_exit)
-        
-        vbox = QVBoxLayout(self)
-        vbox.addWidget(self.state)
-        vbox.addLayout(hbox_bottom)
-        self.setLayout(vbox)
-        
-        self.setGeometry(300, 300, 300, 300)
-        self.setWindowTitle('Quail')
-        
-        self.installer.sig_done.connect(self._on_install_done)
-        self.app.aboutToQuit.connect(self._atexit)
-        self.show()
-        
-    def _startInstall(self):
-        self.btn.setDisabled(True)
-        self.btn_exit.setDisabled(True)
-        self.state.setText('Installing...')
-        self.installer.moveToThread(self.thread)
-        self.thread.started.connect(self.installer._runInstall)
-        self.thread.start()
+class InstallerGui(Installer):
     
-    def _exit(self):
-        self._on_exit()
-        self.app.quit()
-    
-    def _on_install_done(self):
-        self.state.setText('Installed')
-        self.btn_exit.setEnabled(True)
-        self._on_exit()
-    
-    def _atexit(self):
-        self._on_exit()
-    
-    def _on_exit(self):
-        if self.thread.isRunning():
-            self.thread.quit()
-            self.thread.wait()
-
-
-class InstallerGui(Installer, QObject):
-    
-    sig_done = pyqtSignal()
-
     def __init__(self, *args, **kwargs):
-        Installer.__init__(self, *args, **kwargs)
-        QObject.__init__(self)
+        super().__init__(*args, **kwargs)
+        self.tk = Tk()
+        self._thread = None
+        
+    def _init_gui(self):
+        self.button_install = Button(self.tk, text="Install", command=self._run_install)
+        self.button_install.pack(side="left", padx=2, pady=2)
         
     def install(self):
-        app = QApplication([])
-        gui = Gui(self, app)
-        app.exec_()
-    
-    def _runInstall(self):
-        Installer.install(self)
-        self.sig_done.emit()
+        self._init_gui()
+        self.tk.protocol("WM_DELETE_WINDOW", self._on_closing)
+        self.tk.mainloop()
 
+    def _on_closing(self):
+        if self._thread is not None and self._thread.is_alive():
+            if not messagebox.askokcancel("Quit", "Do you want to quit?"):
+                return
+        if self._thread is not None:
+            self._thread.join()
+        self.tk.destroy()
+
+    def _run_install(self):
+        if self._thread is not None:
+            return
+        self._thread = Thread(target=super().install)
+        self._thread.start()
