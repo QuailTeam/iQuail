2bce8450a094ad950f373faafe4299c4707d8fa4
Arthur Josso <arthur.josso@epitech.eu>
2018-09-20 20:55:11 +0100

    github bug report button

--- quail/controller/controller_tkinter/errorbox.py
+++ quail/controller/controller_tkinter/errorbox.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import messagebox
 from tkinter.scrolledtext import ScrolledText
 
 class ErrorBox(object):
@@ -19,9 +20,17 @@ class ErrorBox(object):
         text.configure(state="disabled")
         text.pack()
 
-        okButton = tki.Button(frm, text='OK')
-        okButton['command'] = self.top.destroy
-        okButton.pack(padx=4, pady=4)
+        github_button = tki.Button(frm, text='Submit this bug to Github')
+        github_button['command'] = lambda: self._github_report(msg)
+        github_button.pack(padx=4, pady=4)
+
+        ok_button = tki.Button(frm, text='OK')
+        ok_button['command'] = self.top.destroy
+        ok_button.pack(padx=4, pady=4)
+
+    def _github_report(self, msg):
+        #TODO implem
+        messagebox.showinfo("Success", "A Bug Report has been created on Github, thank you and sorry about that!")
 
     def show(self):
         self.top.grab_set()
