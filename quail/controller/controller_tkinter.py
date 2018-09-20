91cf816896d3fca648baee27b614f91516f5f127
Arnaud Aliès <arnaudalies.py@gmail.com>
2018-09-20 15:56:12 +0200

    FrameInProgress

--- quail/controller/controller_tkinter.py
+++ quail/controller/controller_tkinter.py
@@ -48,7 +48,16 @@ class FrameBase(tk.Frame):
 
 
 class FrameValidate(FrameBase):
+    """Frame to ask user to validate an action"""
+
     def __init__(self, parent, controller, question, hook, positive_str="yes"):
+        """
+        :param parent: tkinter parent
+        :param controller: self explanatory
+        :param question: question string for the user
+        :param hook: this hook will be called if user accept
+        :param positive_str: positive answer string
+        """
         super().__init__(parent, controller)
         label = tk.Label(self,
                          text=question,
@@ -85,32 +94,23 @@ class FrameValidateUninstall(FrameValidate):
         self.controller.tk.quit()
 
 
-class FrameInstallFinished(FrameBase):
+class FrameInstallFinished(FrameValidate):
     def __init__(self, parent, controller):
-        super().__init__(parent, controller)
-        label = tk.Label(self,
-                         text="%s successfully installed!" % self.manager.get_name(),
-                         font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10, padx=10)
-
-        button = tk.Button(self,
-                           text="exit",
-                           command=self._exit)
-        button.pack(side="bottom", padx=20, pady=20)
+        super().__init__(parent, controller,
+                         question="%s successfully installed!" %
+                                  controller.manager.get_name(),
+                         hook=self._exit,
+                         positive_str="exit")
 
     def _exit(self):
         self.controller.tk.quit()
 
 
-class FrameUpdating(FrameBase):
-    def __init__(self, parent, controller):
+class FrameInProgress(FrameBase):
+    def __init__(self, parent, controller, label_str):
         super().__init__(parent, controller)
-        self.manager.set_solution_progress_hook(self.progress_callback)
-        self.manager.set_install_part_solution_hook(self.solution_finished_callback)
-
-        self._label = tk.Label(self, text="Updating...", font=controller.title_font)
+        self._label = tk.Label(self, text=label_str, font=controller.title_font)
         self._label.pack(side="top", fill="x", pady=10)
-
         self.progress_var = tk.IntVar()
         self._progress_bar = ttk.Progressbar(self,
                                              orient=tk.HORIZONTAL,
@@ -118,46 +118,47 @@ class FrameUpdating(FrameBase):
                                              mode='determinate',
                                              variable=self.progress_var)
         self._progress_bar.pack(side="bottom", fill="x", padx=20, pady=20)
+
+    def update_label(self, text):
+        self._label.configure(text=text)
+        self._label.update()
+
+    def update_progress(self, percent):
+        if percent < 0:
+            percent = 0
+        if percent > 100:
+            percent = 100
+        self.progress_var.set(percent)
+
+
+class FrameUpdating(FrameInProgress):
+    def __init__(self, parent, controller):
+        super().__init__(parent, controller, "Updating...")
+        self.manager.set_solution_progress_hook(self.progress_callback)
+        self.manager.set_install_part_solution_hook(self.solution_finished_callback)
         thread = self.tk_thread(self.manager.update)
         thread.start()
 
     def progress_callback(self, progress: SolutionProgress):
-        self._label.configure(text=progress.status.capitalize() + " ...")
-        self._label.update()
-        progress = int(progress.percent)
-        if 0 <= progress <= 100:
-            self.progress_var.set(progress)
+        self.update_label(progress.status.capitalize() + " ...")
+        self.update_progress(progress.percent)
 
     def solution_finished_callback(self):
         self.controller.tk.quit()
 
 
-class FrameInstalling(FrameBase):
+class FrameInstalling(FrameInProgress):
     def __init__(self, parent, controller):
-        super().__init__(parent, controller)
+        super().__init__(parent, controller, "Installing...")
         self.manager.set_solution_progress_hook(self.progress_callback)
         self.manager.set_install_finished_hook(self.install_finished_callback)
         self.manager.set_install_part_solution_hook(self.solution_finished_callback)
-
-        self._label = tk.Label(self, text="Installing...", font=controller.title_font)
-        self._label.pack(side="top", fill="x", pady=10)
-
-        self.progress_var = tk.IntVar()
-        self._progress_bar = ttk.Progressbar(self,
-                                             orient=tk.HORIZONTAL,
-                                             length=100,
-                                             mode='determinate',
-                                             variable=self.progress_var)
-        self._progress_bar.pack(side="bottom", fill="x", padx=20, pady=20)
         thread = self.tk_thread(self.manager.install_part_solution)
         thread.start()
 
     def progress_callback(self, progress: SolutionProgress):
-        self._label.configure(text=progress.status.capitalize() + " ...")
-        self._label.update()
-        progress = int(progress.percent)
-        if 0 <= progress <= 100:
-            self.progress_var.set(progress)
+        self.update_label(progress.status.capitalize() + " ...")
+        self.update_progress(progress.percent)
 
     def solution_finished_callback(self):
         self.controller.tk.after(0, self.manager.install_part_register)
