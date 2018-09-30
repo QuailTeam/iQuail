c2ed4afc12ea4cb2dcacf5e8944b0d591fd802f0
Arnaud Aliès <arnaudalies.py@gmail.com>
2018-09-30 15:19:24 +0200

    configuration wip

--- quail/helper/configuration.py
+++ quail/helper/configuration.py
@@ -5,23 +5,26 @@ from pathlib import Path
 class Configuration:
     """This is an helper to save and load configuration"""
     def __init__(self, filename):
-        Path(filename).touch()
         self._filename = filename
         self._parser = configparser.ConfigParser()
-        self._parser.read(filename)
-        default_scope = "DEFAULT"
-        if default_scope not in self._parser:
-            self._parser[default_scope] = {}
-        self._config = self._parser[default_scope]
+        self._default_scope = "DEFAULT"
+        self._config = {}
 
-    def _save(self):
+    def read(self):
+        Path(self._filename).touch()
+        self._parser.read(self._filename)
+        if self._default_scope not in self._parser:
+            self._parser[self._default_scope] = {}
+        self._config = self._parser[self._default_scope]
+
+    def save(self):
+        self._parser[self._default_scope] = self._config
         with open(self._filename, "w") as f:
             self._parser.write(f)
 
     def set(self, name, value):
         assert value is not None
         self._config[name] = value
-        self._save()
 
     def get(self, name, default=None):
         value = self._config.get(name, None)
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -8,9 +8,12 @@ class TestConfiguration(BaseTestCase):
     def test_set_get_1(self):
         c = Configuration(self.tmp("test.ini"))
         c.set("test", "test_value")
+        c.save()
         c = Configuration(self.tmp("test.ini"))
+        c.read()
         self.assertEqual(c.get("test"), "test_value")
 
     def test_no_value(self):
         c = Configuration(self.tmp("test.ini"))
+        c.read()
         self.assertEqual(c.get("test", default=1), 1)
