4f274a38004a2164fed57098c0e736e5f73223a9
Arnaud Aliès <arnaudalies.py@gmail.com>
2018-10-09 22:12:57 +0200

    wip pip

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 """Setup generator file."""
-from distutils.core import setup
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+requirements = open("requirements.txt").read().splitlines()
 
 setup(
     name='quail',
     packages=['quail'],
-    version='1.0',
-    description='Quail cross-platform install builder',
+    version='1.2',
+    description='Quail cross-platform installer',
     author='Quail team',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='quail_2020@labeip.epitech.eu',
-    url='https://github.com/mouuff/Quail',
-    download_url='https://github.com/mouuff/Quail/tarball/master',
+    url='https://github.com/QuailTeam/Quail',
     keywords=['tool', 'deploy', 'installer', 'wizard', 'install', 'update'],
     classifiers=['Intended Audience :: Developers',
-                 'Development Status :: 1 - Planning',
+                 'Development Status :: 3 - Alpha',
                  'Programming Language :: Python :: 3 :: Only',
                  'Topic :: Software Development'
                  ],
     install_requires=[
         'pyinstaller'
     ],
-    entry_points={
-        'console_scripts': [
-            'quail = quail.__main__:main'
-        ]
-    },
 )
