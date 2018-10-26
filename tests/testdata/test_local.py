6c7f1dd602c536834d9d08fbac86d49a09e40a90
khatep <jay.jindani@epitech.eu>
2018-10-26 20:08:05 +0200

    added test_local.py

---
+++ tests/testdata/test_local.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os
+import os.path
+import iquail
+import platform
+
+if iquail.helper.OS_LINUX:
+    solution_path = ['Allum1']
+    iquail.run(
+        iquail.SolutionLocal(['Allum1']),
+        iquail.Installer(
+            name='Allum1',
+            icon='icon.jpeg',
+            binary='allum1',
+            console=True
+        ),
+        iquail.builder.Builder(
+            iquail.builder.CmdIntegrity(solution_path)
+        )
+    )
+if iquail.helper.OS_WINDOWS:
+    solution_path = ['OpenHardwareMonitor']
+    iquail.run(
+        iquail.SolutionLocal(['OpenHardwareMonitor']),
+        iquail.Installer(
+            name='OpenHardwareMonitor',
+            icon='OpenHardwareMonitor.exe',
+            binary='OpenHardwareMonitor.exe',
+            console=True
+        ),
+        iquail.builder.Builder(
+            iquail.builder.CmdIntegrity(solution_path)
+        )
+    )
