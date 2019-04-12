367abac27ffe768596670c3a394a88a184367c82
Artous <thibautartis@pm.me>
2019-04-12 14:10:35 -0700

    a bit of test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,9 +1,12 @@
 import shutil
 import iquail
 import unittest
+import sys
 import os
+import ctypes
 from .base_test_case import BaseTestCase
-from iquail.helper import misc
+from iquail.helper import misc, Constants
+from unittest.mock import MagicMock
 
 
 class TestMisc(BaseTestCase):
@@ -27,4 +30,28 @@ class TestMisc(BaseTestCase):
                 self.assertListEqual(self.verifier.verify_all(), [])
         finally:
             os.chmod(subfolder, 0o777)
+
+
+    def test_running_from_installed_binary(self):
+        misc.get_script_path = lambda: "/test/test/test/test"
+        assert misc.running_from_installed_binary() is False
+        misc.get_script_path = lambda: "/test/"
+        assert misc.running_from_installed_binary() is False
+        misc.get_script_path = lambda: os.path.join("/test/test/", Constants.IQUAIL_ROOT_NAME, "test")
+        assert misc.running_from_installed_binary() is True
+
+
+    def test_rerun_as_admin(self):
+        global mock
+        if misc.OS_LINUX:
+            os.execvp = MagicMock()
+            mock = os.execvp
+        elif misc.OS_WINDOWS:
+            ctypes.windll.shell32.ShellExecuteW = MagicMock()
+            mock = ctypes.windll.shell32.ShellExecuteW
+        sys.argv = ['./test.py']
+        misc.rerun_as_admin(False)
+        mock.assert_called_with('sudo', ['sudo', './test.py'])
+        misc.rerun_as_admin(True, '/dir/', '/bin')
+        mock.assert_called_with('pkexec', ['pkexec', '/bin', '--iquail_path', '/dir/'])
 
