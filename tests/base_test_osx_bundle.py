8532d48c10fe305b78f012adb71e9811459b70d1
PEAU Clement <clement.peau@epitech.eu>
2019-10-24 08:56:23 +0200

    Skipping IOS test on different platforms

--- tests/base_test_osx_bundle.py
+++ tests/base_test_osx_bundle.py
@@ -1,17 +1,20 @@
-import shutil
+import iquail.helper
 
-from tests.base_test_case import BaseTestCase
-from iquail.helper.osx.bundle_template import BundleTemplate
-from iquail.helper.osx.plist_creator import PlistCreator
+if iquail.helper.OS_OSX:
+    import shutil
+
+    from tests.base_test_case import BaseTestCase
+    from iquail.helper.osx.bundle_template import BundleTemplate
+    from iquail.helper.osx.plist_creator import PlistCreator
 
 
-class BaseTestOsxBundle(BaseTestCase):
-    # The test folder will always be named test.app
-    def setUp(self):
-        super(BaseTestOsxBundle, self).setUp()
-        self._test_folder_path = self.tmp('test.app')
-        self.bt = BundleTemplate('test', self._tmpdir)
-        self.bt.make()
+    class BaseTestOsxBundle(BaseTestCase):
+        # The test folder will always be named test.app
+        def setUp(self):
+            super(BaseTestOsxBundle, self).setUp()
+            self._test_folder_path = self.tmp('test.app')
+            self.bt = BundleTemplate('test', self._tmpdir)
+            self.bt.make()
 
-    def tearDown(self):
-        shutil.rmtree(self.bt.full_path, ignore_errors=True)
+        def tearDown(self):
+            shutil.rmtree(self.bt.full_path, ignore_errors=True)
--- tests/test_bundle_osx.py
+++ tests/test_bundle_osx.py
@@ -1,19 +1,22 @@
-import os
+import iquail.helper
 
-from iquail.helper.osx.plist_creator import PlistCreator
-from tests.base_test_osx_bundle import BaseTestOsxBundle
+if iquail.helper.OS_OSX:
+    import os
+
+    from iquail.helper.osx.plist_creator import PlistCreator
+    from tests.base_test_osx_bundle import BaseTestOsxBundle
 
 
-class TestOsxBundle(BaseTestOsxBundle):
-    """
+    class TestOsxBundle(BaseTestOsxBundle):
+        """
         In this test we make sure that the proper folder has the right name and that all the subfolders are created correctly
-    """
-    def test_default_bundle_setup(self):
-        # Bundle template create a folder appended with .app
-        assert os.path.exists(self._test_folder_path)
-        assert os.path.exists(os.path.join(self._test_folder_path, 'Contents'))
+        """
+        def test_default_bundle_setup(self):
+            # Bundle template create a folder appended with .app
+            assert os.path.exists(self._test_folder_path)
+            assert os.path.exists(os.path.join(self._test_folder_path, 'Contents'))
 
-    def test_plist_creation(self):
-        plist_creator = PlistCreator('test', self._tmpdir)
-        plist_creator.write_file()
-        assert os.path.exists(os.path.join(self._test_folder_path, 'Contents', 'info.plist'))
+        def test_plist_creation(self):
+            plist_creator = PlistCreator('test', self._tmpdir)
+            plist_creator.write_file()
+            assert os.path.exists(os.path.join(self._test_folder_path, 'Contents', 'info.plist'))
