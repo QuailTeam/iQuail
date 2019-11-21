1883974d170ce7a60bcd928d244e9f8d3d793081
alies <alies@localhost.localdomain>
2019-11-21 08:59:15 +0100

    Validate side img only when building

--- iquail/builder/builder.py
+++ iquail/builder/builder.py
@@ -30,7 +30,6 @@ class Builder:
             assert os.path.basename(
                 side_img_override) == Constants.SIDE_IMG_NAME
             self._side_img = side_img_override
-            _validate_side_img(side_img_override)
         self._build_cmds = list(build_cmds)
 
     def register(self, builder_action):
@@ -55,6 +54,7 @@ class Builder:
         return params
 
     def pre_build(self):
+        _validate_side_img(self._side_img)
         for build_cmd in self._build_cmds:
             build_cmd.pre_build()
 
